35a73d5f07fd2ca10d29deff81e39654a73ff8f4
sriharieetyala <sriharieetyala143@gmail.com>
2025-11-23 14:44:19 +0530

    Initial commit

---
+++ src/test/java/com/Flightapp/FlightAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Flightapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
